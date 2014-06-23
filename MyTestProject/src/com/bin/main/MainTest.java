a55ab712ba8b70a7b8afa6513491f4e6a25c9069
bin1001 <januarytop10@gmail.com>
2014-06-23 14:07:37 +0700

    commit

--- MyTestProject/src/com/bin/main/MainTest.java
+++ MyTestProject/src/com/bin/main/MainTest.java
@@ -5,6 +5,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("Test..1234");
 		System.out.println("Cek..1234");
+		System.out.println("Garuda di dadaku");
 	}
 	
 }
