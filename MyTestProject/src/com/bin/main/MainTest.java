b8b43b3ac33d1045538da2533f903cbd00b49f2f
hafidzirsyad <pandawa5>
2014-06-23 14:12:54 +0700

    s

--- MyTestProject/src/com/bin/main/MainTest.java
+++ MyTestProject/src/com/bin/main/MainTest.java
@@ -5,6 +5,8 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("Test..1234");
 		System.out.println("Cek..1234");
+		System.out.println("Garuda di dadaku");
+		System.out.println("Dewa 19");
 	}
 	
 }
